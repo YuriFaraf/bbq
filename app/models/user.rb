8394409858fb524061c0f19b522e374d5478f5f9
YuriFaraf <yura96@gmail.com>
2020-06-16 14:16:55 +0500

    bootstrap installed and views rebuilded

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 40}
+  validates :email, presence: true, uniqueness: true, format: {with: /.+@.+\..+/}
 end
