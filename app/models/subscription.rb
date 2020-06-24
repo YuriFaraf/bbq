782fa57651faaf06250849560548f62ff6aa702e
YuriFaraf <yura96@gmail.com>
2020-06-25 03:11:31 +0500

    validates eventmaker subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,7 +9,8 @@ class Subscription < ApplicationRecord
 
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
-  validates :user_email, exclusion: { in: User.pluck(:email)}, on: :create, unless: -> { user.present? }
+  validates :user_email, exclusion: { in: User.pluck(:email), message: :no_sub_existeduser}, on: :create, unless: -> { user.present? }
+  validate :not_eventmaker, on: :create
 
   def user_name
     if user.present?
@@ -25,5 +26,9 @@ class Subscription < ApplicationRecord
     else
       super
     end
+  end
+
+  def not_eventmaker
+    errors.add(:user, :no_sub_eventmaker) if user == event.user
   end
 end
